f47afdfb3bd7f1c587647df0297ded17c8681044
Kurisu003 <74920220+Kurisu003@users.noreply.github.com>
2021-04-18 23:56:40 +0200

    Code ... wait for it ........... cleanup

--- src/View/Window.java
+++ src/View/Window.java
@@ -9,17 +9,23 @@ public class Window {
 
     public Window(int width, int height, String title, Game game){
         JFrame frame = new JFrame(title);
-        //Setting the size of the window
+        // Setting the size of the window
         frame.setPreferredSize(new Dimension(width, height));
         frame.setMaximumSize(new Dimension(width, height));
         frame.setMinimumSize(new Dimension(width, height));
 
-        frame.add(game);//we add the game class to the frame
+        // we add the game class to the frame
+        frame.add(game);
 
-        frame.setResizable(false);//you cannot resize the window
+        // you cannot resize the window
+        frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLocationRelativeTo(null);//frame is in middle of the window
-        frame.setVisible(true);//let us see the window
+
+        // frame is in middle of the window
+        frame.setLocationRelativeTo(null);
+        
+        // lets us see the window
+        frame.setVisible(true);
         frame.toFront();
         frame.requestFocus();
     }
