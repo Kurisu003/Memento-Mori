9271c4d1f3b445be55e0fddc636c6bdf09bab8ff
Peter Costadoi <74654354+ChefPeter@users.noreply.github.com>
2021-04-28 09:04:20 +0200

    door fixed

--- src/Model/Game.java
+++ src/Model/Game.java
@@ -31,7 +31,7 @@ public class Game extends Canvas implements Runnable {
         camera = new Camera(3264,1728);
         this.addKeyListener(new KeyInput(handler));
 //      this.addMouseListener(new Controller.MouseInput(handler,camera));
-        folder="Anger";
+        folder="Lust";
         BufferedImageLoader loader = new BufferedImageLoader();
 
         new Thread(new Music("res/music/bg_music.wav", ID.BG_music)).start();
