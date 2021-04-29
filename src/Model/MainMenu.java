c4f09d70831c1e8df2acf4cfd1fa4b53238f6d3f
Peter Costadoi <74654354+ChefPeter@users.noreply.github.com>
2021-04-29 09:52:58 +0200

    Settings Update

--- src/Model/MainMenu.java
+++ src/Model/MainMenu.java
@@ -15,6 +15,11 @@ public class MainMenu {
     private BufferedImage startNewGame;
     private BufferedImage continueGame;
     private BufferedImage settings;
+    private BufferedImage saveIcon;
+    private BufferedImage musicVolume;
+    private BufferedImage gameVolume;
+    private BufferedImage gameSoundBar;
+    private BufferedImage musicSoundBar;
 
     public MainMenu(){
     }
@@ -29,6 +34,11 @@ public class MainMenu {
         startNewGame = loader.loadImage("../StartNewGame.png");
         continueGame = loader.loadImage("../ContinueGame.png");
         settings = loader.loadImage("../Settings.png");
+        saveIcon = loader.loadImage("../SaveIcon.png");
+        musicVolume = loader.loadImage("../MusicVolume.png");
+        gameVolume = loader.loadImage("../GameVolume.png");
+        musicSoundBar = loader.loadImage("../GameVolume.png");
+        gameSoundBar = loader.loadImage("../GameVolume.png");
     }
 
     public void render(Graphics g){
@@ -41,6 +51,9 @@ public class MainMenu {
         g.drawImage(startNewGame, 10 + 1088, 300, null);
         g.drawImage(continueGame, 10 + 1088, 375, null);
         g.drawImage(settings, 10 + 1088, 450, null);
+        //g.drawImage(saveIcon, 10 + 1088, 750, null);
+        g.drawImage(musicVolume, 10 + 1088, 743, null);
+        g.drawImage(gameVolume, 10 + 1088, 937, null);
 
         g2d.translate(camera.getX(), camera.getY());
     }
