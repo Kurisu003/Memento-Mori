d01f7837d36386b95c4926c9fd7b290538fd7cc2
Kurisu003 <dani003@outlook.com>
2021-05-05 14:44:22 +0200

    Added ability for Smart enemies to do damage properly and added animation for smart enemires

--- src/Model/SmartEnemy.java
+++ src/Model/SmartEnemy.java
@@ -15,6 +15,7 @@ public class SmartEnemy extends GameObject{
 
     int frameCounter=0;
     int animationCounter=0;
+    boolean isAnimating = false;
 
     public SmartEnemy(int x, int y, ID id, Handler1 handler) {
         super(x, y, id);
@@ -22,8 +23,8 @@ public class SmartEnemy extends GameObject{
         sprites = new ArrayList<BufferedImage>();
 
         BufferedImageLoader loader = new BufferedImageLoader();
-        for(int i=0; i <= 10; i++)
-            sprites.add(loader.loadImage("../Enemies/SmartEnemy/EnemySwing" + i + ".png"));
+        for(int i = 1; i <= 20; i++)
+            sprites.add(loader.loadImage("../Enemies/SmartEnemy/Sprite (" + i + ").png").getSubimage(130, 22,80,72));
 
         displayedImage = sprites.get(0);
     }
@@ -31,6 +32,7 @@ public class SmartEnemy extends GameObject{
     @Override
     public void tick() {
         frameCounter++;
+
         for(GameObject temp: handler.objects){
 
             if(temp.getId() == ID.Block) {
@@ -57,11 +59,31 @@ public class SmartEnemy extends GameObject{
                 }
 
                 // For hitting animation
-                if (this.getBounds().intersects(temp.getBounds())) {
+                if (this.getBounds().intersects(temp.getBounds()) || isAnimating) {
                     animationCounter++;
-                    displayedImage = sprites.get((animationCounter / 11) % 11);
+                    displayedImage = sprites.get((animationCounter / 5) % 20);
+                    // isAnimating is needed so that animation finishes playing
+                    // if player steps out of hitbox
+                    isAnimating = true;
+                    // Needed so that enemy stands still while hitting player
+                    velX += velX * -1;
+                    velY += velY * -1;
+
+                    // Needed so animationcounter doesn't overflow
+                    if(animationCounter >= 100){
+                        animationCounter = 0;
+                    }
+                    // Needed to time when damage needs to be done to player
+                    if(animationCounter >= 40 && animationCounter <= 70){
+                        Dante.getInstance().doAction(1);
+                    }
                 }
-                }else if(temp.getId()== ID.SmartEnemy && temp.hashCode() != this.hashCode()){
+                // If animationcounter has reached its finish,
+                // is animating is set to false
+                if(((animationCounter / 5)  % 20) == 0){
+                    isAnimating = false;
+                }
+                }else if(temp.getId() == ID.SmartEnemy && temp.hashCode() != this.hashCode()){
                     if(frameCounter%20==0) {
                         if (getBoundsBigger().intersects(temp.getBounds())) {
                             x += (velX * 2) * -1;
