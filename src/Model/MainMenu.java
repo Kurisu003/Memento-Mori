465f51dd899c1f714dcf8cb8ba4c7ad10155fe11
Peter Costadoi <74654354+ChefPeter@users.noreply.github.com>
2021-05-11 17:57:59 +0200

    Update MainMenu.java

--- src/Model/MainMenu.java
+++ src/Model/MainMenu.java
@@ -95,9 +95,9 @@ public class MainMenu extends MouseAdapter {
         // To check for Music Volume
         System.out.println();
         if(mx >= 1538 && mx <= 1588 && my >= 743 && my <= 793)
-            Music.setMusicVolume((float) (Music.getMusicVolume() + Math.log(Math.abs(Music.getMusicVolume())) * 3));
+            Music.setMusicVolume((float) (Music.getMusicVolume() + Math.log(Math.abs(Music.getMusicVolume())) * 2));
         if(mx >= 1418 && mx <= 1468 && my >= 743 && my <= 793)
-            Music.setMusicVolume((float) (Music.getMusicVolume() - Math.log(Math.abs(Music.getMusicVolume())) * 3));
+            Music.setMusicVolume((float) (Music.getMusicVolume() - Math.log(Math.abs(Music.getMusicVolume())) * 2));
 
         // To check for Sound volume
         if(mx >= 1538 && mx <= 1588 && my >= 943 && my <= 993)
