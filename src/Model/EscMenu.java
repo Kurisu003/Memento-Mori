42f1a3f87f4a19b453a492a546ba33950e851456
annaksrr <annaksrr@users.noreply.github.com>
2021-05-22 15:53:37 +0200

    Music always plays from the beginning

--- src/Model/EscMenu.java
+++ src/Model/EscMenu.java
@@ -136,12 +136,14 @@ public class EscMenu extends MouseAdapter {
             if (mx >= 1118 && mx <= 1618 && my >= -126 && my <= -76) {
                 Camera.getInstance().setX(Camera.getInstance().getX() + 1088);
                 Music.setIsShop(true);
+                Music.setIsMenu(false);
                 Music.getThreadPool().execute(new Music("res/Music/HeavenlySound.wav", ID.Shop_music));
             }
 
             // To get back to esc menu from shop
             if (mx >= 2168 && mx <= 2411 && my >= -566 && my <= -516){
                 Music.setIsShop(false);
+                Music.setIsMenu(true);
                 Camera.getInstance().setX(Camera.getInstance().getX() - 1088);
             }
 
